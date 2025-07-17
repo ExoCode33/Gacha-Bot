dff43e1faab311e67081c816f59a6ed09563c5d0
ExoCode33 <emmanuel@supracode.ca>
2025-07-16 21:47:59 -0400

    Update fruits-cp.js

--- src/data/fruits-cp.js
+++ src/data/fruits-cp.js
@@ -1,341 +1,141 @@
-// src/data/fruit-cp.js - Devil Fruit Combat Power Multipliers
-const FRUIT_CP_MULTIPLIERS = {
-    // COMMON FRUITS (1.0x - 1.5x)
-    'fruit_001': 1.1, // Bomu Bomu no Mi
-    'fruit_002': 1.2, // Bara Bara no Mi
-    'fruit_003': 1.0, // Sube Sube no Mi
-    'fruit_004': 1.3, // Kilo Kilo no Mi
-    'fruit_005': 1.2, // Doru Doru no Mi
-    'fruit_006': 1.4, // Baku Baku no Mi
-    'fruit_007': 1.1, // Ori Ori no Mi
-    'fruit_008': 1.3, // Bane Bane no Mi
-    'fruit_009': 1.2, // Noro Noro no Mi
-    'fruit_010': 1.4, // Doa Doa no Mi
-    'fruit_011': 1.0, // Awa Awa no Mi
-    'fruit_012': 1.1, // Beri Beri no Mi
-    'fruit_013': 1.2, // Sabi Sabi no Mi
-    'fruit_014': 1.3, // Shari Shari no Mi
-    'fruit_015': 1.5, // Yomi Yomi no Mi
-    'fruit_016': 1.4, // Kage Kage no Mi
-    'fruit_017': 1.3, // Horo Horo no Mi
-    'fruit_018': 1.2, // Suke Suke no Mi
-    'fruit_019': 1.3, // Choki Choki no Mi
-    'fruit_020': 1.0, // Woshu Woshu no Mi
-    'fruit_021': 1.5, // Fuwa Fuwa no Mi
-    'fruit_022': 1.2, // Mato Mato no Mi
-    'fruit_023': 1.4, // Buki Buki no Mi
-    'fruit_024': 1.3, // Guru Guru no Mi
-    'fruit_025': 1.1, // Beta Beta no Mi
-    'fruit_026': 1.2, // Hira Hira no Mi
-    'fruit_027': 1.3, // Ishi Ishi no Mi
-    'fruit_028': 1.1, // Nui Nui no Mi
-    'fruit_029': 1.4, // Giro Giro no Mi
-    'fruit_030': 1.2, // Ato Ato no Mi
-    'fruit_031': 1.3, // Jake Jake no Mi
-    'fruit_032': 1.4, // Mira Mira no Mi
-    'fruit_033': 1.3, // Bisu Bisu no Mi
-    'fruit_034': 1.2, // Pero Pero no Mi
-    'fruit_035': 1.1, // Bata Bata no Mi
-    'fruit_036': 1.2, // Shibo Shibo no Mi
-    'fruit_037': 1.5, // Memo Memo no Mi
-    'fruit_038': 1.3, // Buku Buku no Mi
-    'fruit_039': 1.1, // Kuri Kuri no Mi
-    'fruit_040': 1.2, // Tama Tama no Mi
-    'fruit_041': 1.3, // Kame Kame no Mi
-    'fruit_042': 1.2, // Mushi Mushi no Mi, Model: Kabutomushi
-    'fruit_043': 1.3, // Mushi Mushi no Mi, Model: Suzumebachi
-    'fruit_044': 1.2, // Poke Poke no Mi
-    'fruit_045': 1.1, // Kuku Kuku no Mi
-    'fruit_046': 1.4, // Gocha Gocha no Mi
-    'fruit_047': 1.2, // Hiso Hiso no Mi
-    'fruit_048': 1.3, // Mini Mini no Mi
-    'fruit_049': 1.4, // Ton Ton no Mi
-    'fruit_050': 1.5, // Mero Mero no Mi
-};
+// CP Multipliers for each fruit
+const fruitMultipliers = {
+    // Common Fruits (1.0x base)
+    "Gomu Gomu no Mi": 1.0,
+    "Kilo Kilo no Mi": 1.0,
+    "Chop Chop no Mi": 1.0,
+    "Slip Slip no Mi": 1.0,
+    "Boom Boom no Mi": 1.0,
+    "Wax Wax no Mi": 1.0,
+    "Spring Spring no Mi": 1.0,
+    "Dice Dice no Mi": 1.0,
+    "Spin Spin no Mi": 1.0,
+    "Jacket Jacket no Mi": 1.0,
+    "Berry Berry no Mi": 1.0,
+    "Rust Rust no Mi": 1.0,
+    "Wheel Wheel no Mi": 1.0,
+    "Wash Wash no Mi": 1.0,
+    "Brush Brush no Mi": 1.0,
+    "Swim Swim no Mi": 1.0,
+    "Arms Arms no Mi": 1.0,
+    "Munch Munch no Mi": 1.0,
+    "Bind Bind no Mi": 1.0,
+    "Biscuit Biscuit no Mi": 1.0,
+    "Cream Cream no Mi": 1.0,
+    "Butter Butter no Mi": 1.0,
+    "Book Book no Mi": 1.0,
+    "Memo Memo no Mi": 1.0,
+    "Mirror Mirror no Mi": 1.0,
+    "Slow Slow no Mi": 1.0,
+    "Sick Sick no Mi": 1.0,
+    "Glare Glare no Mi": 1.0,
+    "Millet Millet no Mi": 1.0,
+    "Puff Puff no Mi": 1.0,
+    "Calm Calm no Mi": 1.0,
+    "Cage Cage no Mi": 1.0,
+    "Clank Clank no Mi": 1.0,
+    "Pocket Pocket no Mi": 1.0,
+    "Costume Costume no Mi": 1.0,
+    "Cheer Cheer no Mi": 1.0,
+    "Lick Lick no Mi": 1.0,
+    "Smelt Smelt no Mi": 1.0,
+    "Eat Eat no Mi": 1.0,
+    "Cooking Cooking no Mi": 1.0,
 
-// UNCOMMON FRUITS (1.5x - 2.5x)
-const UNCOMMON_FRUITS = {
-    'fruit_051': 2.5, // Gomu Gomu no Mi
-    'fruit_052': 2.1, // Hana Hana no Mi
-    'fruit_053': 2.3, // Bari Bari no Mi
-    'fruit_054': 1.9, // Nagi Nagi no Mi
-    'fruit_055': 2.4, // Hobi Hobi no Mi
-    'fruit_056': 1.8, // Sui Sui no Mi
-    'fruit_057': 2.0, // Zou Zou no Mi
-    'fruit_058': 2.1, // Inu Inu no Mi, Model: Wolf
-    'fruit_059': 2.2, // Neko Neko no Mi, Model: Leopard
-    'fruit_060': 2.0, // Ushi Ushi no Mi, Model: Bison
-    'fruit_061': 1.9, // Ushi Ushi no Mi, Model: Giraffe
-    'fruit_062': 2.1, // Inu Inu no Mi, Model: Jackal
-    'fruit_063': 2.3, // Tori Tori no Mi, Model: Falcon
-    'fruit_064': 1.7, // Mogu Mogu no Mi
-    'fruit_065': 1.6, // Inu Inu no Mi, Model: Dachshund
-    'fruit_066': 1.8, // Sara Sara no Mi, Model: Axolotl
-    'fruit_067': 2.4, // Ryu Ryu no Mi, Model: Allosaurus
-    'fruit_068': 2.5, // Ryu Ryu no Mi, Model: Spinosaurus
-    'fruit_069': 2.3, // Ryu Ryu no Mi, Model: Pteranodon
-    'fruit_070': 2.4, // Ryu Ryu no Mi, Model: Brachiosaurus
-    'fruit_071': 2.5, // Ryu Ryu no Mi, Model: Mammoth
-    'fruit_072': 2.4, // Ryu Ryu no Mi, Model: Triceratops
-    'fruit_073': 2.2, // Ryu Ryu no Mi, Model: Pachycephalosaurus
-    'fruit_074': 2.3, // Ryu Ryu no Mi, Model: Saber-tooth Tiger
-    'fruit_075': 2.1, // Kumo Kumo no Mi, Model: Rosamygale Grauvogeli
-};
+    // Uncommon Fruits (1.1x base)
+    "Spike Spike no Mi": 1.1,
+    "Hollow Hollow no Mi": 1.1,
+    "Barrier Barrier no Mi": 1.1,
+    "Hobby Hobby no Mi": 1.1,
+    "Paw Paw no Mi": 1.1,
+    "Clear Clear no Mi": 1.1,
+    "Flower Flower no Mi": 1.1,
+    "Revive Revive no Mi": 1.1,
+    "Shadow Shadow no Mi": 1.1,
+    "Chilly Chilly no Mi": 1.1,
+    "Smoke Smoke no Mi": 1.1,
+    "Flame Flame no Mi": 1.1,
+    "Sand Sand no Mi": 1.1,
+    "Rumble Rumble no Mi": 1.1,
+    "Dark Dark no Mi": 1.1,
+    "Love Love no Mi": 1.1,
+    "Venom Venom no Mi": 1.1,
+    "Warp Warp no Mi": 1.1,
+    "Scissor Scissor no Mi": 1.1,
+    "Clone Clone no Mi": 1.1,
+    "Straw Straw no Mi": 1.1,
+    "Heal Heal no Mi": 1.1,
+    "Glint Glint no Mi": 1.1,
+    "Magnet Magnet no Mi": 1.1,
+    "Castle Castle no Mi": 1.1,
+    "Float Float no Mi": 1.1,
+    "Mark Mark no Mi": 1.1,
+    "String String no Mi": 1.1,
+    "Poke Poke no Mi": 1.1,
+    "Ope Ope no Mi": 1.1,
 
-// RARE FRUITS (2.5x - 4.0x)
-const RARE_FRUITS = {
-    'fruit_076': 3.2, // Moku Moku no Mi
-    'fruit_077': 3.8, // Mera Mera no Mi
-    'fruit_078': 3.5, // Suna Suna no Mi
-    'fruit_079': 4.0, // Goro Goro no Mi
-    'fruit_080': 3.9, // Hie Hie no Mi
-    'fruit_081': 3.7, // Yami Yami no Mi
-    'fruit_082': 4.0, // Pika Pika no Mi
-    'fruit_083': 3.9, // Magu Magu no Mi
-    'fruit_084': 3.1, // Numa Numa no Mi
-    'fruit_085': 3.3, // Gasu Gasu no Mi
-    'fruit_086': 3.4, // Yuki Yuki no Mi
-    'fruit_087': 3.6, // Mori Mori no Mi
-    'fruit_088': 3.8, // Soru Soru no Mi
-    'fruit_089': 3.7, // Mochi Mochi no Mi
-    'fruit_090': 4.0, // Ope Ope no Mi
-    'fruit_091': 3.9, // Gura Gura no Mi
-    'fruit_092': 3.5, // Kaze Kaze no Mi
-    'fruit_093': 3.6, // Denki Denki no Mi
-    'fruit_094': 3.2, // Tetsu Tetsu no Mi
-    'fruit_095': 3.4, // Kin Kin no Mi
-    'fruit_096': 3.3, // Tsuchi Tsuchi no Mi
-    'fruit_097': 3.5, // Kuki Kuki no Mi
-    'fruit_098': 3.1, // Chi Chi no Mi
-    'fruit_099': 3.2, // Kori Kori no Mi
-    'fruit_100': 3.8, // Mizu Mizu no Mi
-};
+    // Rare Fruits (1.3x base)
+    "Human Human no Mi": 1.3,
+    "Ox Ox no Mi": 1.3,
+    "Falcon Falcon no Mi": 1.3,
+    "Mole Mole no Mi": 1.3,
+    "Dog Dog no Mi": 1.3,
+    "Elephant Elephant no Mi": 1.3,
+    "Giraffe Giraffe no Mi": 1.3,
+    "Horse Horse no Mi": 1.3,
+    "Cat Cat no Mi": 1.3,
+    "Snake Snake no Mi": 1.3,
+    "Turtle Turtle no Mi": 1.3,
+    "Hippo Hippo no Mi": 1.3,
+    "Rhino Rhino no Mi": 1.3,
+    "Spider Spider no Mi": 1.3,
+    "Insect Insect no Mi": 1.3,
+    "Salamander Salamander no Mi": 1.3,
+    "Axolotl Axolotl no Mi": 1.3,
+    "Deer Deer no Mi": 1.3,
+    "Cow Cow no Mi": 1.3,
+    "Sheep Sheep no Mi": 1.3,
+    "Pig Pig no Mi": 1.3,
+    "Chicken Chicken no Mi": 1.3,
+    "Eagle Eagle no Mi": 1.3,
+    "Hawk Hawk no Mi": 1.3,
+    "Owl Owl no Mi": 1.3,
+    "Rabbit Rabbit no Mi": 1.3,
+    "Rat Rat no Mi": 1.3,
+    "Monkey Monkey no Mi": 1.3,
+    "Bear Bear no Mi": 1.3,
+    "Lion Lion no Mi": 1.3,
+    "Tiger Tiger no Mi": 1.3,
 
-// EPIC FRUITS (4.0x - 6.0x)
-const EPIC_FRUITS = {
-    'fruit_101': 5.8, // Hito Hito no Mi, Model: Daibutsu
-    'fruit_102': 5.5, // Zushi Zushi no Mi
-    'fruit_103': 5.7, // Nikyu Nikyu no Mi
-    'fruit_104': 5.0, // Doku Doku no Mi
-    'fruit_105': 4.8, // Horm Horm no Mi
-    'fruit_106': 4.5, // Chyu Chyu no Mi
-    'fruit_107': 6.0, // Toki Toki no Mi
-    'fruit_108': 4.7, // Kibi Kibi no Mi
-    'fruit_109': 5.2, // Juku Juku no Mi
-    'fruit_110': 5.1, // Basu Basu no Mi
-    'fruit_111': 5.3, // Rei Rei no Mi
-    'fruit_112': 5.9, // Kuukan Kuukan no Mi
-    'fruit_113': 5.6, // Sei Sei no Mi
-    'fruit_114': 5.4, // Kokoro Kokoro no Mi
-    'fruit_115': 5.8, // Gensou Gensou no Mi
-};
+    // Epic Fruits (1.6x base)
+    "Ice Ice no Mi": 1.6,
+    "Magma Magma no Mi": 1.6,
+    "Swamp Swamp no Mi": 1.6,
+    "Gas Gas no Mi": 1.6,
+    "Snow Snow no Mi": 1.6,
+    "Mud Mud no Mi": 1.6,
+    "Candy Candy no Mi": 1.6,
+    "Mochi Mochi no Mi": 1.6,
+    "Tremor Tremor no Mi": 1.6,
+    "Soul Soul no Mi": 1.6,
+    "Time Time no Mi": 1.6,
+    "Gravity Gravity no Mi": 1.6,
+    "Push Push no Mi": 1.6,
+    "Repel Repel no Mi": 1.6,
+    "Slow Slow no Mi": 1.6,
+    "Slip Slip no Mi": 1.6,
+    "Smooth Smooth no Mi": 1.6,
+    "Spike Spike no Mi": 1.6,
+    "Hollow Hollow no Mi": 1.6,
+    "Barrier Barrier no Mi": 1.6,
+    "Hobby Hobby no Mi": 1.6,
 
-// LEGENDARY FRUITS (6.0x - 8.0x)
-const LEGENDARY_FRUITS = {
-    'fruit_116': 8.0, // Uo Uo no Mi, Model: Seiryu
-    'fruit_117': 7.5, // Inu Inu no Mi, Model: Okuchi no Makami
-    'fruit_118': 7.8, // Hebi Hebi no Mi, Model: Yamata-no-Orochi
-    'fruit_119': 7.9, // Tori Tori no Mi, Model: Phoenix
-    'fruit_120': 7.2, // Hito Hito no Mi, Model: Onyudo
-    'fruit_121': 7.6, // Inu Inu no Mi, Model: Nine-Tailed Fox
-    'fruit_122': 7.3, // Uma Uma no Mi, Model: Pegasus
-    'fruit_123': 7.4, // Batto Batto no Mi, Model: Vampire
-    'fruit_124': 8.0, // Shi Shi no Mi
-    'fruit_125': 7.9, // Jikan Jikan no Mi
-    'fruit_126': 7.7, // Mugen Mugen no Mi
-    'fruit_127': 7.8, // Zero Zero no Mi
-    'fruit_128': 7.5, // Akuma Akuma no Mi
-    'fruit_129': 7.6, // Tenshi Tenshi no Mi
-    'fruit_130': 7.9, // Ryuu Ryuu no Mi, Model: Eastern Dragon
-};
-
-// MYTHICAL FRUITS (8.0x - 10.0x)
-const MYTHICAL_FRUITS = {
-    'fruit_131': 10.0, // Hito Hito no Mi, Model: Nika
-    'fruit_132': 9.5, // Hito Hito no Mi, Model: Raijin
-    'fruit_133': 9.7, // Hito Hito no Mi, Model: Susanoo
-    'fruit_134': 9.8, // Kami Kami no Mi, Model: Amaterasu
-    'fruit_135': 9.6, // Kami Kami no Mi, Model: Tsukuyomi
-    'fruit_136': 9.2, // Tora Tora no Mi, Model: White Tiger
-    'fruit_137': 9.3, // Kitsune Kitsune no Mi, Model: Kyubi
-    'fruit_138': 9.9, // Shinigami Shinigami no Mi
-    'fruit_139': 9.4, // Yume Yume no Mi
-    'fruit_140': 9.8, // Kuro Kuro no Mi
-    'fruit_141': 9.9, // Shiro Shiro no Mi
-    'fruit_142': 9.1, // Henka Henka no Mi
-    'fruit_143': 9.2, // Yuugou Yuugou no Mi
-    'fruit_144': 9.3, // Bunretsu Bunretsu no Mi
-    'fruit_145': 9.5, // Zouryoku Zouryoku no Mi
-};
-
-// OMNIPOTENT FRUITS (10.0x - 12.0x)
-const OMNIPOTENT_FRUITS = {
-    'fruit_146': 12.0, // Kami Kami no Mi
-    'fruit_147': 11.5, // Sekai Sekai no Mi
-    'fruit_148': 11.8, // Uchuu Uchuu no Mi
-    'fruit_149': 11.9, // Sonzai Sonzai no Mi
-    'fruit_150': 11.7  // Subete Subete no Mi
-};
-
-// Combine all fruits into main multipliers object
-Object.assign(FRUIT_CP_MULTIPLIERS, UNCOMMON_FRUITS, RARE_FRUITS, EPIC_FRUITS, LEGENDARY_FRUITS, MYTHICAL_FRUITS, OMNIPOTENT_FRUITS);
-
-// Export fruit arrays for devil-fruits.js
-const commonFruits = Object.keys(FRUIT_CP_MULTIPLIERS).filter(id => parseInt(id.split('_')[1]) <= 50);
-const uncommonFruits = Object.keys(UNCOMMON_FRUITS);
-const rareFruits = Object.keys(RARE_FRUITS);
-const epicFruits = Object.keys(EPIC_FRUITS);
-const legendaryFruits = Object.keys(LEGENDARY_FRUITS);
-const mythicalFruits = Object.keys(MYTHICAL_FRUITS);
-const omnipotentFruits = Object.keys(OMNIPOTENT_FRUITS);
-
-// CP Ranges by rarity
-const CP_RANGES = {
-    common: { min: 1.0, max: 1.5 },
-    uncommon: { min: 1.5, max: 2.5 },
-    rare: { min: 2.5, max: 4.0 },
-    epic: { min: 4.0, max: 6.0 },
-    legendary: { min: 6.0, max: 8.0 },
-    mythical: { min: 8.0, max: 10.0 },
-    omnipotent: { min: 10.0, max: 12.0 }
-};
-
-// Get fruit CP multiplier
-function getFruitCP(fruitId) {
-    return FRUIT_CP_MULTIPLIERS[fruitId] || 1.0;
-}
-
-// Get fruit CP as integer (for database storage)
-function getFruitCPAsInt(fruitId) {
-    const cp = getFruitCP(fruitId);
-    return Math.floor(cp * 100); // Store as integer (multiply by 100)
-}
-
-// Convert integer back to CP decimal
-function intToCP(intValue) {
-    return intValue / 100;
-}
-
-// Get CP range for rarity
-function getCPRange(rarity) {
-    return CP_RANGES[rarity] || CP_RANGES.common;
-}
-
-// Get fruits by CP range
-function getFruitsByCPRange(minCP, maxCP) {
-    return Object.entries(FRUIT_CP_MULTIPLIERS)
-        .filter(([id, cp]) => cp >= minCP && cp <= maxCP)
-        .map(([id]) => id);
-}
-
-// Get top CP fruits
-function getTopCPFruits(limit = 10) {
-    return Object.entries(FRUIT_CP_MULTIPLIERS)
-        .sort(([,a], [,b]) => b - a)
-        .slice(0, limit)
-        .map(([id, cp]) => ({ id, cp }));
-}
-
-// Calculate total CP for user
-function calculateTotalCP(baseCp, fruits) {
-    let totalCp = baseCp;
-    
-    // Group fruits by ID to handle duplicates
-    const fruitGroups = {};
-    fruits.forEach(fruit => {
-        if (!fruitGroups[fruit.id]) {
-            fruitGroups[fruit.id] = {
-                cp: getFruitCP(fruit.id),
-                count: 0
-            };
-        }
-        fruitGroups[fruit.id].count++;
-    });
-    
-    // Calculate CP with duplicates
-    Object.values(fruitGroups).forEach(group => {
-        const duplicateBonus = 1 + ((group.count - 1) * 0.01); // 1% per duplicate
-        const fruitCp = (baseCp * group.cp) * duplicateBonus;
-        totalCp += fruitCp;
-    });
-    
-    return Math.floor(totalCp);
-}
-
-// Calculate duplicate bonus
-function calculateDuplicateBonus(duplicateCount) {
-    return 1 + ((duplicateCount - 1) * 0.01); // 1% per duplicate
-}
-
-// Get CP statistics
-function getCPStats() {
-    const cps = Object.values(FRUIT_CP_MULTIPLIERS);
-    return {
-        total: cps.length,
-        average: cps.reduce((a, b) => a + b, 0) / cps.length,
-        min: Math.min(...cps),
-        max: Math.max(...cps),
-        median: cps.sort((a, b) => a - b)[Math.floor(cps.length / 2)]
-    };
-}
-
-// Get random CP for rarity
-function getRandomCPForRarity(rarity) {
-    const range = getCPRange(rarity);
-    return Math.random() * (range.max - range.min) + range.min;
-}
-
-// Get rarity from CP value
-function getRarityFromCP(cp) {
-    for (const [rarity, range] of Object.entries(CP_RANGES)) {
-        if (cp >= range.min && cp <= range.max) {
-            return rarity;
-        }
-    }
-    return 'common';
-}
-
-// Validate CP for rarity
-function isValidCP(cp, rarity) {
-    const range = getCPRange(rarity);
-    return cp >= range.min && cp <= range.max;
-}
-
-// Get CP tier description
-function getCPTierDescription(cp) {
-    if (cp >= 10.0) return 'Omnipotent';
-    if (cp >= 8.0) return 'Mythical';
-    if (cp >= 6.0) return 'Legendary';
-    if (cp >= 4.0) return 'Epic';
-    if (cp >= 2.5) return 'Rare';
-    if (cp >= 1.5) return 'Uncommon';
-    return 'Common';
-}
-
-module.exports = {
-    // Fruit arrays for devil-fruits.js
-    commonFruits,
-    uncommonFruits,
-    rareFruits,
-    epicFruits,
-    legendaryFruits,
-    mythicalFruits,
-    omnipotentFruits,
-    
-    // CP system
-    FRUIT_CP_MULTIPLIERS,
-    CP_RANGES,
-    getFruitCP,
-    getFruitCPAsInt,
-    intToCP,
-    getCPRange,
-    getFruitsByCPRange,
-    getTopCPFruits,
-    calculateTotalCP,
-    calculateDuplicateBonus,
-    getCPStats,
-    getRandomCPForRarity,
-    getRarityFromCP,
-    isValidCP,
-    getCPTierDescription
-};
+    // Legendary Fruits (2.0x base)
+    "Phoenix Phoenix no Mi": 2.0,
+    "Buddha Buddha no Mi": 2.0,
+    "Dragon Dragon no Mi": 2.0,
+    "Mythical Zoan Model: Phoenix": 2.0,
+    "Mythical Zoan Model: Buddha": 2.0,
+    "Mythical Zoan Model: Dragon": 2.0,
+    "Mythical Zoan Model: Yamata-no-Orochi": 2.0,
+    "Mythical Zoan Model: Nine-Tailed Fox": 2.0,
